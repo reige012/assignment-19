e9794adf6d82e4801f64b4208c80a30592e99387
OSCAR JOHNSON <OJOHNS7@TIGERS.LSU.EDU>
2016-04-18 21:55:03 -0500

    update

--- answers/henicorhina/amniote_analyze.py
+++ answers/henicorhina/amniote_analyze.py
@@ -12,8 +12,10 @@ returns NCBI taxonomy as csv file
 """
 
 import os
+import pdb
 import argparse
 import pandas as pd
+import numpy as np
 
 
 def get_args():
@@ -36,21 +38,37 @@ def amniote(args):
     """
 
     """
+    # import file
     df = pd.read_csv(args.file)
-    
+    df = df.replace(-999, np.nan)
+    # sets of classes and orders
+    classes = set(df['class'])
+    order = set(df['order'])
+    # l = df[(df['class']=='Aves')]
+    outfile = open('results.csv', 'w')
+    for tax in classes:
+        div_by_class = df[(df['class']==tax)]
+        # pdb.set_trace()
+        for taxon in order:
+            if taxon in div_by_class['order']:
+                div_by_order = div_by_class[(div_by_class['order']==taxon)]
+                # print(div_by_order)
+                for p in args.parameters:
+                    r = list(div_by_order[p].describe())
+                    info = [tax, taxon, p]
+                    out_list = info + [r[1], (r[2]*2), r[3], r[7], r[5]]
+                    print(out_list)
+                    # div_by_order 
+                    outfile.write(out_list, '\n')
+    outfile.close()
 
 
 def main():
     args = get_args()
-    # rename outfile to .fasta if needed
-    if args.out_file[-4:] != '.csv':
-        args.out_file += '.csv'
-    else:
-        pass
     os.chdir(os.path.dirname(args.file))
-    amniotes(args)
-    l = [class,order,parameter,mean(parameter),
-        95CI(parameter),min(parameter),max(parameter),median(parameter)]
+    amniote(args)
+    # pars = [class,order,parameter,mean(parameter),
+    #     95CI(parameter),min(parameter),max(parameter),median(parameter)]
 
 if __name__ == '__main__':
     main()
