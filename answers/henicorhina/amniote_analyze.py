dfe232675ba1dd57eb284f30b1f099db219f3c52
OSCAR JOHNSON <OJOHNS7@TIGERS.LSU.EDU>
2016-04-18 23:07:54 -0500

    working

--- answers/henicorhina/amniote_analyze.py
+++ answers/henicorhina/amniote_analyze.py
@@ -8,11 +8,16 @@ Oscar Johnson 18 April 2016
 Copyright Oscar Johnson 2016
 
 provide input txt file of amniote life history database
-returns NCBI taxonomy as csv file
+returns statistics in csv file
+
+input should be in the form of the column headers that
+you want statistics for,
+divided by commas and with no spaces in between
 """
 
 import os
 import pdb
+import csv
 import argparse
 import pandas as pd
 import numpy as np
@@ -36,39 +41,49 @@ def get_args():
 
 def amniote(args):
     """
-
+    parses input pandas DataFrame and returns
+    various statistics
+    writes these to a csv file
     """
+    pars = ['class', 'order', 'parameter', 'mean(parameter)',
+            '95CI(parameter)', 'min(parameter)', 'max(parameter)',
+            'median(parameter)']
     # import file
     df = pd.read_csv(args.file)
     df = df.replace(-999, np.nan)
     # sets of classes and orders
     classes = set(df['class'])
     order = set(df['order'])
-    # l = df[(df['class']=='Aves')]
-    outfile = open('results.csv', 'w')
-    for tax in classes:
-        div_by_class = df[(df['class']==tax)]
-        # pdb.set_trace()
-        for taxon in order:
-            if taxon in div_by_class['order']:
-                div_by_order = div_by_class[(div_by_class['order']==taxon)]
-                # print(div_by_order)
-                for p in args.parameters:
-                    r = list(div_by_order[p].describe())
-                    info = [tax, taxon, p]
-                    out_list = info + [r[1], (r[2]*2), r[3], r[7], r[5]]
-                    print(out_list)
-                    # div_by_order 
-                    outfile.write(out_list, '\n')
-    outfile.close()
+    # pdb.set_trace()
+    with open('results.csv', 'w') as outfile:
+        writer = csv.writer(outfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(pars)
+        for tax in classes:
+            div_by_class = df[(df['class'] == tax)]
+            # pdb.set_trace()
+            # print(div_by_class['order'])
+            for taxon in order:
+                # pdb.set_trace()
+                if taxon in set(list(div_by_class['order'])):
+                    # pdb.set_trace()
+                    div = div_by_class[(div_by_class['order'] == taxon)]
+                    # pdb.set_trace()
+                    for val in args.parameters:
+                        # pdb.set_trace()
+                        r = list(div[val].describe())
+                        info = [tax, taxon, val]
+                        out_list = info + [str(r[1]), str((r[2]*2)), str(r[3]),
+                                           str(r[7]), str(r[5])]
+                        # pdb.set_trace()
+                        writer.writerow(out_list)
 
 
 def main():
     args = get_args()
     os.chdir(os.path.dirname(args.file))
+    args.parameters = args.parameters.split(',')
+    # pdb.set_trace()
     amniote(args)
-    # pars = [class,order,parameter,mean(parameter),
-    #     95CI(parameter),min(parameter),max(parameter),median(parameter)]
 
 if __name__ == '__main__':
     main()
